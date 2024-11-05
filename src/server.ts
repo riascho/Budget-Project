ddd80b6730f10034046455789f009822e497064d
Ria <123465523+riascho@users.noreply.github.com>
2024-11-05 11:37:04 +0530

    init frontend setup

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import Express, { RequestHandler } from "express";
 import { envelopeRouter } from "./routers/envelope-router";
+import path from "path";
 
 const app = Express();
 const PORT = "3000";
 
 app.use(Express.json());
+
+app.use(Express.static(path.join(__dirname, "../templates")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "templates", "index.html"));
+});
 
 app.use("/envelopes", envelopeRouter);
 
