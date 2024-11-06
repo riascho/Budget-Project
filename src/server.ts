5fb57a1d7917587bee475eff60c4f5b104a7330d
Ria <123465523+riascho@users.noreply.github.com>
2024-11-06 12:59:56 +0530

    adds basic error handling

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,9 @@
-import Express, { RequestHandler } from "express";
+import Express, {
+  RequestHandler,
+  Request,
+  Response,
+  NextFunction,
+} from "express";
 import { envelopeRouter } from "./routers/envelope-router";
 import path from "path";
 
@@ -9,15 +14,44 @@ app.use(Express.json());
 
 app.use(Express.static(path.join(__dirname, "../templates")));
 
+// Middleware to log requests
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(`${req.method} Request to ${req.url}`);
+  next(); // Pass control to the next middleware
+});
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "templates", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontEnd/", "index.html"));
 });
 
 app.use("/envelopes", envelopeRouter);
 
+// Route to simulate a 500 error
+app.get("/error", (req, res) => {
+  throw new Error("Simulated server error");
+});
+
+// 404 Handler
 app.use((req, res) => {
   res.status(404).send(`Endpoint '${req.url}' not Found`);
 });
+
+// in express only 500 are errors
+// for it to recognize a middleware as error handler
+// it needs the 'err' parameter first and 'next' parameter present
+
+// Error Handler
+const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+};
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
