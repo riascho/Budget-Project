f0730a89ae3d1210922782d9fa5a04be00621ef7
Ria <123465523+riascho@users.noreply.github.com>
2025-03-11 17:02:50 +0100

    prepares for tests

---
+++ src/db/db.test.ts
@@ -0,0 +1,4 @@
+describe("Database connection", () => {
+  it("should create the ENVELOPES table if it doesn't yet exist", async () => {});
+  it("should create the TRANSACTIONS table if it doesn't yet exist", async () => {});
+});
