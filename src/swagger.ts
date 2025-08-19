ff348b26817c4cc4a929746c13f848049a3be78b
Ria <123465523+riascho@users.noreply.github.com>
2025-08-19 18:48:21 +0200

    update swagger to use dynamic host from env file

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,6 +1,10 @@
 import swaggerJSDoc from "swagger-jsdoc"; // scans files specified in options.apis
 import swaggerUi from "swagger-ui-express";
 import { Express } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const HOST = process.env.PGHOST;
 
 const options: swaggerJSDoc.Options = {
   definition: {
@@ -13,7 +17,7 @@ const options: swaggerJSDoc.Options = {
     },
     servers: [
       {
-        url: "http://localhost:3000",
+        url: `http://${HOST}:3000`,
         description: "Development server",
       },
     ],
@@ -89,5 +93,5 @@ const swaggerSpec = swaggerJSDoc(options);
 
 export const setupSwagger = (app: Express): void => {
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-  console.log("Swagger UI available at http://localhost:3000/api-docs");
+  console.log(`Swagger UI available at http://${HOST}:3000/api-docs`);
 };
